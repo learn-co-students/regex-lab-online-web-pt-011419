72545f014ef41f19f2fffe9182ae3b9669af9e16
Ryan Mui <reachryanmui@gmail.com>
2019-04-01 01:50:22 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,12 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  check_vowel = false
+  if(word.scan(/\A[aeiou]/) != nil)
+    binding.pry
+    check_vowel = true
+  end
+  check_vowel
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
