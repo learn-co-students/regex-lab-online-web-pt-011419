a11dd065e8399dde52c4f34f68280af95b0e05cb
Ryan Mui <reachryanmui@gmail.com>
2019-04-03 01:31:59 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,15 +2,17 @@ require 'pry'
 
 def starts_with_a_vowel?(word)
   check_vowel = false
-  if(word.scan(/\A[aeiou]/) != nil)
-    binding.pry
+  if(!word.scan(/^[aeiouAEIOU]/).empty?)
     check_vowel = true
   end
+  # binding.pry
   check_vowel
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  array = []
+  array << text.scan(//)
+  array
 end
 
 def words_five_letters_long(text)
